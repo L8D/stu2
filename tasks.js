f0936cdfa4600fd7a924cf1a64c3c8f568ac052a
Tenor Biel <tenorbiel@gmail.com>
2018-11-23 22:03:19 -0600

    Move example tasks into shared tasks module

---
+++ tasks.js
@@ -0,0 +1,66 @@
+module.exports = [
+  {
+    deadline: '7am',
+    objective: 'wake up'
+  },
+
+  {
+    deadline: '7:30am',
+    objective: 'take theanine'
+  },
+
+  {
+    deadline: '8am',
+    objective: 'shower'
+  },
+
+  {
+    deadline: '9am',
+    objective: 'meditate'
+  },
+
+  {
+    deadline: '12pm',
+    objective: 'get out'
+  },
+
+  {
+    deadline: '1pm',
+    objective: 'eat lunch'
+  },
+
+  {
+    deadline: '1:05pm',
+    objective: 'log lunch'
+  },
+
+  {
+    deadline: '6pm',
+    objective: 'form a plan for getting home and eating dinner'
+  },
+
+  {
+    deadline: '8pm',
+    objective: 'go home and eat dinner'
+  },
+
+  {
+    deadline: '8:20pm',
+    objective: 'write'
+  },
+
+  {
+    deadline: '8:40pm',
+    objective: 'draw'
+  },
+
+  {
+    deadline: '9pm',
+    objective: 'wash'
+  },
+
+  {
+    deadline: '9:30pm',
+    objective: 'meditate'
+  }
+]
