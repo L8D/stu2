cfe1c873513670c0c711d4d3c9fc7bc3afbcc7e2
Tenor Biel <tenorbiel@gmail.com>
2019-02-07 11:06:21 -0500

    new schedule

--- schedules.js
+++ schedules.js
@@ -1,4 +1,14 @@
 exports.timezone = 'America/New_York'
+
+const goals = [
+  'anki',
+  'stu',
+  'ghostwriter',
+  'infrastructure',
+  'dat',
+  'sudomesh'
+]
+
 exports.daily = [
   {
     deadline: '6am',
@@ -6,28 +16,48 @@ exports.daily = [
   },
 
   {
-    deadline: '6:30am',
+    deadline: '6:15',
     objective: 'take theanine'
   },
 
   {
-    deadline: '7am',
+    deadline: '6:30am',
     objective: 'wash'
   },
 
   {
-    deadline: '7:30am',
+    deadline: '7:45am',
     objective: 'meditate'
   },
 
   {
     deadline: '8am',
-    objective: 'read RC wiki'
+    objective: `hack on ${goals.pop()}`
+  },
+
+  {
+    deadline: '8:15am',
+    objective: `hack on ${goals.pop()}`
+  },
+
+  {
+    deadline: '8:30am',
+    objective: 'get ready leave'
+  },
+
+  {
+    deadline: '8:55am',
+    objective: 'catch the 8:55am C train towards 168 St'
+  },
+
+  {
+    deadline: '10:15am',
+    objective: `hack on ${goals.pop()}`
   },
 
   {
     deadline: '11am',
-    objective: 'get outside'
+    objective: `hack on ${goals.pop()}`
   },
 
   {
@@ -46,37 +76,57 @@ exports.daily = [
   },
 
   {
-    deadline: '3pm',
-    objective: 'form a plan for getting home and eating dinner'
+    deadline: '1:45pm',
+    objective: `hack on ${goals.pop()}`
+  },
+
+  {
+    deadline: '2:45pm',
+    objective: 'participate in PLP'
+  },
+
+  {
+    deadline: '5:00pm',
+    objective: `hack on ${goals.pop()}`
+  },
+
+  {
+    deadline: '5:15pm',
+    objective: 'form a plan for eating dinner'
   },
 
   {
     deadline: '6:30pm',
-    objective: 'go home and eat dinner'
+    objective: 'eat dinner'
   },
 
   {
-    deadline: '7:00pm',
-    objective: 'write'
-  },
-
-  {
-    deadline: '7:20pm',
-    objective: 'draw'
-  },
-
-  {
-    deadline: '7:40pm',
-    objective: 'wash'
+    deadline: '7:30pm',
+    objective: 'go home'
   },
 
   {
     deadline: '8:00pm',
+    objective: 'write'
+  },
+
+  {
+    deadline: '8:20pm',
+    objective: 'draw'
+  },
+
+  {
+    deadline: '8:40pm',
+    objective: 'wash'
+  },
+
+  {
+    deadline: '9:00pm',
     objective: 'meditate'
   },
 
   {
-    deadline: '8:30pm',
+    deadline: '9:30pm',
     objective: 'read a book'
   }
 ]
