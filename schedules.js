f23324f4d239e96285d0ac6420b6354a3e5e1a57
Tenor Biel <tenorbiel@gmail.com>
2019-04-23 22:12:10 -0400

    update schedule

--- schedules.js
+++ schedules.js
@@ -1,110 +1,35 @@
 exports.timezone = 'America/New_York'
 
-const goals = [
-  'anki',
-  'stu',
-  'ghostwriter',
-  'infrastructure',
-  'dat; hyperledger fabric',
-  'sudomesh'
-]
-
 exports.daily = [
   {
-    deadline: '6am',
+    deadline: '7am',
     objective: 'wake up'
   },
 
   {
-    deadline: '6:15',
-    objective: 'take theanine'
+    deadline: '7:15am',
+    objective: 'theanine, agmatine, curcumin'
   },
 
   {
-    deadline: '6:30am',
+    deadline: '8:00am',
     objective: 'wash'
   },
 
   {
-    deadline: '7:45am',
-    objective: 'meditate'
+    deadline: '10:00am',
+    objective: '10g protein; 10g fat'
   },
 
   {
-    deadline: '8am',
-    objective: `hack on ${goals.pop()}`
+    deadline: '4:00pm',
+    objective: 'be prepared to stop eating'
   },
 
   {
-    deadline: '8:15am',
-    objective: `hack on ${goals.pop()}`
+    deadline: '6:00pm',
+    objective: 'stop eating'
   },
-
-  {
-    deadline: '8:30am',
-    objective: 'get ready leave'
-  },
-
-  {
-    deadline: '8:55am',
-    objective: 'catch the 8:55am C train towards 168 St'
-  },
-
-  {
-    deadline: '10:15am',
-    objective: `hack on ${goals.pop()}`
-  },
-
-  {
-    deadline: '11am',
-    objective: `hack on ${goals.pop()}`
-  },
-
-  {
-    deadline: '12pm',
-    objective: 'plan lunch'
-  },
-
-  {
-    deadline: '1pm',
-    objective: 'eat lunch'
-  },
-
-  {
-    deadline: '1:05pm',
-    objective: 'log lunch'
-  },
-
-  {
-    deadline: '1:45pm',
-    objective: `hack on ${goals.pop()}`
-  },
-
-  {
-    deadline: '2:45pm',
-    objective: 'participate in PLP'
-  },
-
-  {
-    deadline: '5:00pm',
-    objective: `hack on ${goals.pop()}`
-  },
-
-  {
-    deadline: '5:15pm',
-    objective: 'form a plan for eating dinner'
-  },
-
-  {
-    deadline: '6:30pm',
-    objective: 'eat dinner'
-  },
-
-  {
-    deadline: '7:30pm',
-    objective: 'go home'
-  },
-
   {
     deadline: '8:00pm',
     objective: 'write'
